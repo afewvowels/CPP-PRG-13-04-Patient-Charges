8a81355d09a307e33f62c7b7a9f57777aa5c6bd8
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-15 13:08:05 -0500

    Prompt written.

--- CPP-PRG-13-04-Patient-Charges/main.cpp
+++ CPP-PRG-13-04-Patient-Charges/main.cpp
@@ -5,11 +5,49 @@
 //  Created by Keith Smith on 11/15/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a class named Patient that has member variablrs for the following data:
+//
+//  * First name, middle name, last name
+//  * Address, city, state, and ZIP code
+//  * Phone number
+//  * Name and phone number of emergency contact
+//
+//  The Patient class should have a constructor that accepts an argument for each member
+//  variable. The Patient class should also have accessor and mutator functions for
+//  each member variable.
+//
+//  Next, write a class named Procedure that represents a medical procedure that has
+//  been performed on a patient. The Procedure class should have member variables for
+//  the following data:
+//
+//  * Name of the procedure
+//  * Date of the procedure
+//  * Name of the practitioner who performed the procedure
+//  * Charges for the procedure
+//
+//  The Procedure class should have a constructor that accepts an argument for each
+//  member variable. The Procedure class should also have accessor and mutator functions
+//  for each member variable.
+//
+//  Next, write a program that creates an instance of the Patient class, intialized with
+//  sample data. Then, create three instances of the Procedure class, initialized with the
+//  following data:
+//
+//  Procedure #1:                   Procedure #2:                   Procedure #3:
+//  ---------------------------------------------------------------------------------------
+//  Procedure name: Physical Exam   Procedure Name: X-ray           Procedure name: Blood test
+//  Date: Today's date              Date: Today's date              Date: Today's date
+//  Practitioner: Dr. Irvine        Practitioner: Dr. Jamison       Practitioner: Dr. Smith
+//  Charge: 250.00                  Charge: 500.00                  Charge: 200.00
+//
+//  The program should display the patient's information, information about all three of
+//  the procedures, and the total charges of the three procedures.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
